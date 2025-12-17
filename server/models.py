dabf337e05b5ab64aeac7a7544aac6093fb2808e
Garrett Pfingston <gpfingston@gmail.com>
2025-12-17 16:04:56 -0600

    added logic and passed tests

--- server/models.py
+++ server/models.py
@@ -10,6 +10,29 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', backref='reviews')
+    item = db.relationship('Item', backref='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.rating}>'
+    
+class ReviewSchema(Schema):
+    id = fields.Int()
+    comment = fields.Str()
+    customer_id = fields.Int()
+    item_id = fields.Int()
+
+    customer = fields.Nested('CustomerSchema', only=['id', 'name'])
+    item = fields.Nested('ItemSchema', only=['id', 'name', 'price'])
 
 class Customer(db.Model):
     __tablename__ = 'customers'
@@ -17,9 +40,16 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    items = association_proxy('reviews', 'item')
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
+class CustomerSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+    reviews = fields.Nested('ReviewSchema', many=True, only=['id', 'comment', 'item'])
 
 class Item(db.Model):
     __tablename__ = 'items'
@@ -30,3 +60,10 @@ class Item(db.Model):
 
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class ItemSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    price = fields.Float()
+
+    reviews = fields.Nested('ReviewSchema', many=True, only=['id', 'comment', 'customer'])
